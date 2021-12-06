dc7ed08abeb1b8e7304d3701a8ced84c73c10af4
LUKA <lukalavalferk@gmail.com>
2021-12-06 22:40:59 +0100

    partie 1 ok + debut partie 2

--- src/main.pl
+++ src/main.pl
@@ -4,6 +4,20 @@ programme :-
   deuxieme_etape(Abi,Abi1,Tbox),
   troisieme_etape(Abi1,Abr).
 
+
+
+premiere_etape(Tbox,Abi,Abr) :-
+  make_tbox(Tbox),
+  make_abi(Abi),
+  make_abr(Abr).
+
+make_tbox(Tbox) :- setof((C,D), equiv(C,D), Tbox).
+make_abi(Abi) :- setof((I,C), inst(I,C), Abi).
+make_abr(Abr) :- setof((I1,I2,R), instR(I1,I2,R), Abr).
+
+
+
+
 deuxieme_etape(Abi,Abi1,Tbox) :-
   saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox).
 
@@ -20,6 +34,28 @@ suite(2,Abi,Abi1,Tbox) :-
 suite(R,Abi,Abi1,Tbox) :-
   nl, write('Cette reponse est incorrecte.'),
   nl, saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox).
+
+acquisition_prop_type1(Abi,Abi1,Tbox) :-
+  nl, write('Entrez une instance :'),
+  nl, read(I), instance(I),
+  nl, write('Entrez un concept :'),
+  nl, read(C), concept(C),
+  remplace(C,CC),
+  nnf(not(CC),NCC),
+  Abi1 = [(I,NCC)|Abi].
+
+acquisition_prop_type2(Abi,Abi1,Tbox) :-
+  nl, write('Entrez un concept (1) :'),
+  nl, read(C1), concept(C1),
+  nl, write('Entrez un concept (2) :'),
+  nl, read(C2), concept(C2),
+  remplace(and(C1,C2),CC),
+  nnf(CC,NCC),
+  Abi1 = [(I,NCC)|Abi].
+
+# Pour Abi1 = [(I,NCC)|Abi]. dans acquisition_prop_type2 je suis pas sur, je sais qu'il faut traduire un il existe une instance tel que I : NCC
+# JSP pourquoi on a jamais besoin de la TBOX jusqu'ici et dans la troisieme etape elle est mm pas en parametre donc elle sert à rien ??
+
 
 
 
--- src/pred.pl
+++ src/pred.pl
@@ -1,15 +1,3 @@
-nnf(not(and(C1,C2)),or(NC1,NC2)):- nnf(not(C1),NC1),nnf(not(C2),NC2),!.
-nnf(not(or(C1,C2)),and(NC1,NC2)):- nnf(not(C1),NC1),nnf(not(C2),NC2),!.
-nnf(not(all(R,C)),some(R,NC)) :- nnf(not(C),NC),!.
-nnf(not(some(R,C)),all(R,NC)):- nnf(not(C),NC),!.
-nnf(not(not(X)),X):- !.
-nnf(not(X),not(X)):- !.
-nnf(and(C1,C2),and(NC1,NC2)):- nnf(C1,NC1),nnf(C2,NC2),!.
-nnf(or(C1,C2),or(NC1,NC2)):- nnf(C1,NC1),nnf(C2,NC2),!.
-nnf(some(R,C),some(R,NC)):- nnf(C,NC),!.
-nnf(all(R,C),all(R,NC)) :- nnf(C,NC),!.
-nnf(X,X).
-
 autoref(C,C) :- cnamea(C).
 autoref(C,C) :- cnamena(C).
 autoref(C,D) :- cnamea(C), equiv(D,E), autoref(C,E).
@@ -29,21 +17,43 @@ autoref(C,some(_,D)) :- cnamena(C), autoref(C,D).
 autoref(C,all(_,D)) :- cnamea(C), autoref(C,D).
 autoref(C,all(_,D)) :- cnamena(C), autoref(C,D).
 
-semantique(A) :- cnamea(A).
-semantique(A) :- cnamena(A).
-semantique(not(A)) :- semantique(A).
-semantique(and(A,B)) :- semantique(A), semantique(B).
-semantique(or(A,B)) :- semantique(A), semantique(B).
-semantique(all(R,A)) :- rname(R), semantique(A).
-semantique(some(R,A)) :- rname(R), semantique(A).
-semantique(inst(I,C)) :- iname(I), cnamea(C).
-semantique(inst(I,C)) :- iname(I), cnamena(C).
-semantique(instR(I1,I2,R)) :- iname(I1), iname(I2), rname(R).
+nnf(not(and(C1,C2)),or(NC1,NC2)):- nnf(not(C1),NC1),nnf(not(C2),NC2),!.
+nnf(not(or(C1,C2)),and(NC1,NC2)):- nnf(not(C1),NC1),nnf(not(C2),NC2),!.
+nnf(not(all(R,C)),some(R,NC)) :- nnf(not(C),NC),!.
+nnf(not(some(R,C)),all(R,NC)):- nnf(not(C),NC),!.
+nnf(not(not(X)),X):- !.
+nnf(not(X),not(X)):- !.
+nnf(and(C1,C2),and(NC1,NC2)):- nnf(C1,NC1),nnf(C2,NC2),!.
+nnf(or(C1,C2),or(NC1,NC2)):- nnf(C1,NC1),nnf(C2,NC2),!.
+nnf(some(R,C),some(R,NC)):- nnf(C,NC),!.
+nnf(all(R,C),all(R,NC)) :- nnf(C,NC),!.
+nnf(X,X).
 
 concept(C) :- cnamea(C), !.
 concept(C) :- cnamena(C), !.
 concept(not(C)) :- concept(C), !.
 concept(or(C1,C2)) :- concept(C1), concept(C2), !.
 concept(and(C1,C2)) :- concept(C1), concept(C2), !.
-concept(some(_,C)) :- concept(C), !.
-concept(all(_,C)) :- concept(C), !.
+concept(some(R,C)) :- rname(R), concept(C), !.
+concept(all(R,C)) :- rname(R), concept(C), !.
+
+instance(I) :- iname(I), !.
+
+remplace(C,C) :- cnamea(C).
+remplace(not(C),not(CC)) :- remplace(C,CC).
+remplace(and(C1,C2),and(CC1,CC2)) :- remplace(C1,CC1), remplace(C2,CC2).
+remplace(or(C1,C2),or(CC1,CC2)) :- remplace(C1,CC1), remplace(C2,CC2).
+remplace(some(R,C),some(R,CC)) :- remplace(C,CC).
+remplace(all(R,C),all(R,CC)) :- remplace(C,CC).
+remplace(C,CC) :- equiv(C,D), remplace(D,CC).
+
+# semantique(A) :- cnamea(A).
+# semantique(A) :- cnamena(A).
+# semantique(not(A)) :- semantique(A).
+# semantique(and(A,B)) :- semantique(A), semantique(B).
+# semantique(or(A,B)) :- semantique(A), semantique(B).
+# semantique(all(R,A)) :- rname(R), semantique(A).
+# semantique(some(R,A)) :- rname(R), semantique(A).
+# semantique(inst(I,C)) :- iname(I), cnamea(C).
+# semantique(inst(I,C)) :- iname(I), cnamena(C).
+# semantique(instR(I1,I2,R)) :- iname(I1), iname(I2), rname(R).
